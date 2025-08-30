2087d1aa22e45f6055cd772547f94788d28d7a54
Jean Garcia <cjcarlosgc@outlook.com>
2025-08-30 18:15:14 -0500

    chore: improve of logs

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -5,25 +5,41 @@ export const loggerMiddleware = (
   response: Response,
   next
 ) => {
-  if (request.url !== '/health') {
-    const time: string = new Date().toISOString();
+  const url: string = request.url;
+  const time: string = new Date().toISOString();
 
-    console.log(
-      `[${time}] ${request.method} ${request.url} - IP: ${request.ip}`
-    );
-    // console.log(`Request: ${JSON.stringify(request.param)}`);
-    console.log(`Request query: ${JSON.stringify(request.query)}`);
-    console.log(`Request params: ${JSON.stringify(request.params)}`);
-    console.log(`Request body: ${JSON.stringify(request.body)}`);
-
-    const start: number = Date.now();
-
-    response.on('finish', () => {
-      const duration = Date.now() - start;
-      console.log(`se resolvió en: ${duration}ms`);
-      console.log(`Response: ${response.send()}`);
-    });
-
-    next();
+  if (url !== '/health' && url !== '/') {
+    console.log('**************************************')
   }
+  
+  console.log(
+    `[${time}] ${request.method} ${request.url} - IP: ${request.ip}`
+  );
+
+  console.log(`Request query: ${JSON.stringify(request.query)}`);
+  console.log(`Request params: ${JSON.stringify(request.params)}`);
+  console.log(`Request body: ${JSON.stringify(request.body)}`);
+
+  if (url !== '/health' && url !== '/') {
+    console.log('**************************************')
+  }
+
+  const start: number = Date.now();
+
+  response.on('finish', () => {
+    const duration = Date.now() - start;
+
+    if (url !== '/health' && url !== '/') {
+      console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+    }
+
+    console.log(`se resolvió en: ${duration}ms`);
+    console.log(`Response: ${response.send()}`);
+
+    if (url !== '/health' && url !== '/') {
+      console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+    }
+  });
+
+  next();
 };
